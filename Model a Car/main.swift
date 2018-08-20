64214463b81e80356652a489bda172a39e74752e
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-08-20 14:30:44 -0500

    Initial Commit

---
+++ Model a Car/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Model a Car
+//
+//  Created by Mason Nesbitt on 8/20/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
